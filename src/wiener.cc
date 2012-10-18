9a985bb20a968ff54fef17d15fcb33fd75492956
Dominik Wabersich <dominik.wabersich@gmail.com>
2012-10-17 21:52:23 -0700

    edited makefiles

--- src/wiener.cc
+++ src/wiener.cc
@@ -1,3 +1,26 @@
+/*
+ *  Copyright (C) 2012 Dominik Wabersich <dominik.wabersich@gmail.com>
+ *  and Joachim Vandekerckhove <joachim@uci.edu>
+ *
+ *  When using this module, please cite as: 
+ *      Wabersich, D. and Vandekerckhove, J. (in preparation). Hacking JAGS: 
+ *      Adding custom distributions to JAGS, with a diffusion model example.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ */
 #include <Module.h>
 #include <distributions/DWiener.h>
 
