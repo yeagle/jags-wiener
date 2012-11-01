88584a38f74f79aecdc99063bc1d6aaf2a2d5387
Dominik Wabersich <dominik.wabersich@gmail.com>
2012-11-01 14:07:32 -0700

    Cleaned up example

--- example/R/Rscript.R
+++ example/R/Rscript.R
@@ -2,15 +2,21 @@
 library(rjags)
 load.module("wiener")
 
-# read data and inits
-dat <- read.jagsdata("easytask_data.txt")
-inits1 <- read.jagsdata("inits1.txt")
-inits2 <- read.jagsdata("inits2.txt")
-inits3 <- read.jagsdata("inits3.txt")
+# read original data
+dat <- read.table("data/exp1_01_m23r_2012-10-31-110958.txt", skip=8, header=T)
+dat <- dat[21:218,] # Throw out trainig trials
+dat$RT[dat$Key == "a"] = -dat$RT[dat$Key == "a"]
+dat <- data.frame(Id=dat$Id,Cond=as.numeric(dat$Stim), RT=dat$RT)
+
+# data and inits
+dat <- list(N=198, y=dat$RT, C=3, cond=dat$Cond)
+inits1 <- list(alpha=1, tau=0.001, delta.mu=c(1,-1,-2), delta.si=0.5)
+inits2 <- list(alpha=2, tau=0.003, delta.mu=c(-1,1,0), delta.si=0.8)
+inits3 <- list(alpha=0.2, tau=0.004, delta.mu=c(2,1.5,-2), delta.si=0.2)
 inits <- list(inits1,inits2,inits3)
 
 # create jags model and sample from it
-easy_model <- jags.model("easy_model.txt", dat, inits, n.chains=3)
+easy_model <- jags.model("../easy_model.txt", dat, inits, n.chains=3)
 samples <- jags.samples(easy_model, n.iter=5000, 
                         variable.names=c("alpha", "tau", 
                                          "delta.mu", "delta.si"))
@@ -36,12 +42,15 @@ densplot(alpha, main="Posterior Distribution", xlab="Value", ylab="Density")
 x11() ; par(mfrow=c(1,2))
 traceplot(tau, main="Chains", xlab="n", ylab="Value")
 densplot(tau, main="Posterior Distribution", xlab="Value", ylab="Density")
+# Dark condition
 x11() ; par(mfrow=c(1,2))
 traceplot(delta.mu1, main="Chains", xlab="n", ylab="Value")
 densplot(delta.mu1, main="Posterior Distribution", xlab="Value", ylab="Density")
+# Bright condition
 x11() ; par(mfrow=c(1,2))
 traceplot(delta.mu2, main="Chains", xlab="n", ylab="Value")
 densplot(delta.mu2, main="Posterior Distribution", xlab="Value", ylab="Density")
+# Mixed condition
 x11() ; par(mfrow=c(1,2))
 traceplot(delta.mu3, main="Chains", xlab="n", ylab="Value")
 densplot(delta.mu3, main="Posterior Distribution", xlab="Value", ylab="Density")
